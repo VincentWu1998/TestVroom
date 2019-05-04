5c19d28f88354d5e7e9f04aa99f50ae7fc38ae63
Vincent Zhigang Wu <vincent.z.wu98@gmail.com>
2019-05-04 20:40:00 +0000

    Reversing works slightly, car has more realistic speeds, jittery on startup but fine afterwards

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Crash : MonoBehaviour
-{        
+{
+    public WheelCollider frontDriverW, frontPassengerW;
+    public WheelCollider rearDriverW, rearPassengerW;
+    public Transform frontDriverT, frontPassengerT;
+    public Transform rearDriverT, rearPassengerT;
+
 
     // Use this for initialization
     void Start()
@@ -25,6 +30,11 @@ public class Crash : MonoBehaviour
             Vector3 dir = other.contacts[0].point - transform.position;
             dir = -dir.normalized;
             GetComponent<Rigidbody>().AddForce(dir * 100000);
+
+            frontPassengerW.motorTorque = 0;
+            frontDriverW.motorTorque = 0;
+            rearDriverW.motorTorque = 0;
+            rearPassengerW.motorTorque = 0;
         }
     }
 }
