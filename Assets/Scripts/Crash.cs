3bb403cb397cc5e5f502b36ebf871e84088b90eb
Vincent Zhigang Wu <vincent.z.wu98@gmail.com>
2019-05-13 02:44:17 +0000

    Collisions work and its counters work, car still lurches forward after collision.

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -9,6 +9,8 @@ public class Crash : MonoBehaviour
     public WheelCollider rearDriverW, rearPassengerW;
     public Transform frontDriverT, frontPassengerT;
     public Transform rearDriverT, rearPassengerT;
+	
+	public int collisionCounter = 0;
 
 
     // Use this for initialization
@@ -29,12 +31,15 @@ public class Crash : MonoBehaviour
         {
             Vector3 dir = other.contacts[0].point - transform.position;
             dir = -dir.normalized;
-            GetComponent<Rigidbody>().AddForce(dir * 100000);
-
+            GetComponent<Rigidbody>().AddForce(dir * 500000);			
+			collisionCounter++;
+			Debug.Log("We have collided " + collisionCounter + " times!");		
+			RealisticCarController.m_verticalInput = 0;
             frontPassengerW.motorTorque = 0;
             frontDriverW.motorTorque = 0;
             rearDriverW.motorTorque = 0;
-            rearPassengerW.motorTorque = 0;
+            rearPassengerW.motorTorque = 0;			
+			
         }
     }
 }
