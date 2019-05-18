18cd81b07e85d554ca898f955b2a347a39642a2a
Christian Lay-Geng <christianlay@me.com>
2019-05-18 21:57:41 +0000

    Crash script to control collision counter, deleted fences

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -9,9 +9,10 @@ public class Crash : MonoBehaviour
     public WheelCollider rearDriverW, rearPassengerW;
     public Transform frontDriverT, frontPassengerT;
     public Transform rearDriverT, rearPassengerT;
-	
 	public int collisionCounter = 0;
-
+	private float waitTime = 5.0F;
+	private float nextFire = 0.0F;
+	private bool crashed = false;
 
     // Use this for initialization
     void Start()
@@ -22,24 +23,36 @@ public class Crash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+		if (crashed == true && Time.time > nextFire) {
+			StartCoroutine(addScore ());
+			Debug.Log ("got here");
+			Debug.Log (crashed);
+			Debug.Log (collisionCounter);
+		}
     }
 
     void OnCollisionEnter(Collision other)
-    {
+	{
         if (other.gameObject.tag == "collidable")
         {
             Vector3 dir = other.contacts[0].point - transform.position;
             dir = -dir.normalized;
             GetComponent<Rigidbody>().AddForce(dir * 250000);			
-			collisionCounter++;
-			Debug.Log("We have collided " + collisionCounter + " times!");		
+			crashed = true;	
 			RealisticCarController.m_verticalInput = 0;
             frontPassengerW.motorTorque = 0;
             frontDriverW.motorTorque = 0;
             rearDriverW.motorTorque = 0;
             rearPassengerW.motorTorque = 0;			
-			
+
         }
-    }
+	}
+
+	public IEnumerator addScore(){
+		collisionCounter++;
+		nextFire = Time.time + waitTime;
+		Debug.Log ("score is now " + collisionCounter);
+		crashed = false;
+		yield return new WaitForSeconds (waitTime);
+	}
 }
